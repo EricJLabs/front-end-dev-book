48168069ab130ff9ad11c8c9e245aaa7b7d426fc
Eric <HeyEricJ@gmail.com>
2016-10-31 22:22:29 -0700

    silver (#40)

---
+++ chapter 13 - Challenges/coffeerun/scripts/main.js
@@ -0,0 +1,26 @@
+(function (window, truckId) {
+  'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+  var App = window.App;
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+  var RemoteDataStore = App.RemoteDataStore;
+  var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
+  var CheckList = App.CheckList;
+  var remoteDS = new RemoteDataStore(SERVER_URL);
+  window.remoteDS = remoteDS;
+  var myTruck = new Truck(truckId, remoteDS);//new DataStore());
+  window.myTruck = myTruck;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+  var formHandler = new FormHandler(FORM_SELECTOR);
+  formHandler.addSubmitHandler(function (data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
+  formHandler.addInputHandler(Validation.isCompanyEmail);
+  console.log(formHandler);
+})(window, 'Java Express by HAL Inc.');
